bf0fdb1a32bbcd4e9479a0008caa0c76218ddccf
David <David@David-PC>
2015-09-01 17:42:27 -0700

    Added Logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -22,11 +22,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     post login_path, session: {email: @user.email, password: 'password'}
     assert_redirected_to @user
     follow_redirect!
+    assert is_test_user_logged_in?
     assert_template('users/show', "Not on user home page")
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user) 
   end
   
-  
+test "login followed by logout" do
+   get login_path
+   post login_path, session: {email: @user.email, password: 'password'}
+   delete logout_path
+   assert_redirected_to root_url
+   follow_redirect!
+   assert_not is_test_user_logged_in?
+   assert_select "a[href=?]", login_path
+   assert_select "a[href=?]", logout_path,      count: 0
+   assert_select "a[href=?]", user_path(@user), count: 0 
+ end
+ 
 end
