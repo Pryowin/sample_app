a568b6013fccaf5fa315a990376ef228e7ec07f4
David <David@David-PC>
2015-09-03 16:51:44 -0700

    Add Cookie to enable remaining logged in after browser closes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,30 @@
 module SessionsHelper
-  def login(user)
+  def log_in(user)
     session[:user_id] = user.id
   end
   
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:userid] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   def current_user
-    @current_user = @current_user || User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id]) #Note that this is intentionally using assignment and is not a typo for ==
+      @current_user = @current_user || User.find_by(id: user_id)
+    elsif  (user_id = cookies.signed[:userid])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
   
   def logged_in?
@@ -12,7 +32,8 @@ module SessionsHelper
   end
   
   def log_out
+    forget(current_user)
     session.delete(:user_id)
-    @current_user = nil
+    current_user = nil
   end
 end
