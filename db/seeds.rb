cd35f71d774d7c074b08de5ab9fad8d0e0003667
David <David@David-PC>
2015-09-07 14:13:42 -0700

    Added Delete capability for admin users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#User.create!( name:                   "Example User",
-#              email:                  "example@railstutorial.org",
-#              password:               "foobar",
-#              password_confirmation:  "foobar")
+unless User.find_by( email: "example@railstutorial.org" )
+  User.create( name:                   "Example User",
+               email:                  "example@railstutorial.org",
+               password:               "foobar",
+               password_confirmation:  "foobar",
+               admin:                  true)
+end
               
   99.times do |n|
     name  = Faker::Name.name()
