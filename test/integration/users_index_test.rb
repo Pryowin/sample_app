a69fd03265bb090b41e02f0224eeae28e0466961
David <David@David-PC>
2015-09-12 14:29:09 -0700

    "Finish user edit, update, index, and destroy actions"

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,17 +1,41 @@
 require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
- def setup
-   @user = users(:michael)
- end
+  def setup
+    @admin = users(:michael)
+    @non_admin = users(:archer)
+  end
  
- test "index including pagination" do
-   log_in_as(@user)
-   get users_path
-   assert_template 'users/index'
-   assert_select 'div.pagination'
-   User.paginate(page: 1, :per_page => UsersController::PAGE_SIZE).each do |user|
-     assert_select 'a[href=?]',user_path(user),text: user.name
+  test "index including pagination" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    User.paginate(page: 1, :per_page => UsersController::PAGE_SIZE).each do |user|
+      assert_select 'a[href=?]',user_path(user),text: user.name
+    end
+  end
+ 
+  test "index as admin including pagination and delete" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    User.paginate(page: 1, :per_page => 10).each do |user|
+      assert_select 'a[href=?]',user_path(user),text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]',user_path(user), text: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+       delete user_path(@non_admin)
+    end
    end
- end
+ 
+   test "index as non-admin, no delete" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
+
 end
