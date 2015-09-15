72369aab2a2f5e6a1b9aacc416ca04bd326bcd23
David <David@David-PC>
2015-09-15 15:12:38 -0700

    Refactored code from Controller to Model. Completing account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,15 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+  
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
                         
   #Class methods
                         
