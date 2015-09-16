c7073c06b9e9b48939cfe85f46db39c4c96c21ae
Pryowin <dburke@amberfire.net>
2015-09-16 16:39:46 -0700

    Corrected host name in prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_methid = :smtp
-  host = afternoon-eyrie-9689.herokuapp.com
+  host = 'afternoon-eyrie-9689.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ApplicationMailer::Base.smtp.settings = {
       :address            => 'smtp.sendgrid.net',
