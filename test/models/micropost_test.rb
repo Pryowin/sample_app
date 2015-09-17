38da790236fdf4bdc71cdea1cc525620c4879776
Pryowin <dburke@amberfire.net>
2015-09-17 16:59:51 -0700

    Created many to many relationship with delete of mp on delete of user.

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -34,6 +34,10 @@ class MicropostTest < ActiveSupport::TestCase
   test "Clean 140 characters is valid" do
     @micropost.content = "a" * 140
     assert @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,57 +3,57 @@ require 'test_helper'
 
 
 class UserTest < ActiveSupport::TestCase
-  
+
   MIN_PWD_LEN = User::MIN_PWD_LEN
   MAX_NAME_LEN = User::MAX_NAME_LEN
   MAX_EMAIL_LEN = User::MAX_EMAIL_LEN
-  
+
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
                     password: "foobar", password_confirmation: "foobar")
   end
-  
+
   test "should be valid" do
     assert @user.valid?
   end
-  
+
   test "name should be present" do
     @user.name = " "
     assert_not @user.valid?
   end
-  
+
   test "email should be present" do
     @user.email = ""
     assert_not @user.valid?
   end
-  
+
   test "name should not be too long" do
     @user.name = "a" * (MAX_NAME_LEN + 1)
     assert_not @user.valid?
   end
-  
+
   test "email should not be too long" do
     domain = "@example.com"
     @user.email = "a" * (MAX_EMAIL_LEN - domain.length + 1) + domain
     assert_not @user.valid?
   end
-  
+
   test "email validation should accept valid addresses" do
     validAddresses = %w[user@example.com USER@foo.COM A_Us-er@foo.bar.org first.last@foo.uk alice+bob@baz.cn]
     validAddresses.each do |validAddress|
       @user.email = validAddress
-      assert @user.valid?, "#{validAddress.inspect} should be valid" 
-    end 
+      assert @user.valid?, "#{validAddress.inspect} should be valid"
+    end
   end
-  
+
   test "email validation should reject invalid addresses" do
     invalidAddresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
     invalidAddresses.each do |invalidAddress|
       @user.email = invalidAddress
-      assert_not @user.valid?, "#{invalidAddress.inspect} should be invalid" 
-    end 
+      assert_not @user.valid?, "#{invalidAddress.inspect} should be invalid"
+    end
   end
-  
+
   test "email address should be unique" do
     dupUser = @user.dup
     dupUser.email = @user.email.upcase
@@ -69,11 +69,19 @@ class UserTest < ActiveSupport::TestCase
   test "password should be at least minimum length" do
     @user.password = "a" * (MIN_PWD_LEN - 1)
     @user.password_confirmation = "a" * (MIN_PWD_LEN - 1)
-    
+
     assert_not @user.valid?
   end
-  
+
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem Ipsum")
+    assert_difference 'Micropost.count', - 1 do
+      @user.destroy
+    end
   end
 end
