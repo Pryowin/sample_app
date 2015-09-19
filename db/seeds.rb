7b2d4132c6af3329aebc7b08d02ef2ba8f3e8f86
Pryowin <dburke@amberfire.net>
2015-09-19 12:43:21 -0700

    Added display of microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+
 unless User.find_by( email: "example@railstutorial.org" )
   User.create( name:                   "Example User",
                email:                  "example@railstutorial.org",
                password:               "foobar",
                password_confirmation:  "foobar",
                admin:                  true,
-               activated:              true, 
+               activated:              true,
                activated_at:           Time.zone.now)
 end
-              
+
   99.times do |n|
     name  = Faker::Name.name()
     email = "example-#{n+1}@railstutorial.org"
@@ -26,4 +28,27 @@ end
                   password_confirmation:  password,
                   activated:              true,
                   activated_at:           Time.zone.now)
+  end
+  User.create( name:                   "David Burke",
+               email:                  "dburke@amberfire.net",
+               password:               "foobar",
+               password_confirmation:  "foobar",
+               admin:                  false,
+               activated:              true,
+               activated_at:           Time.zone.now)
+
+
+
+  users = User.order(:created_at).take(5)
+  me = User.find_by(email: "dburke@amberfire.net")
+  if !me.nil?
+    users.push me
+  end
+  50.times do
+
+    content = Faker::Lorem.sentence(5)
+    until !Obscenity.profane?(content) do
+      content = Faker::Lorem.sentence(5)
+    end
+    users.each{ |user| user.microposts.create!(content: content) }
   end
